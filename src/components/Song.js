7b4f8d7a7dd5062ea181a6eb4b1b8a8e010ddd32
BarbaraHernandez <bandezanne@gmail.com>
2020-10-06 22:28:54 -0400

    initial commit

---
+++ src/components/Song.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Song() {
+    return (
+        <li>            
+        </li>
+    )
+}
+
+export default Song
---
+++ src/components/SongView.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SongView() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SongView
